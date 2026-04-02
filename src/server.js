9cb82137d9e98ee4a4bd091eb64da79450fcd6a5
AnuragKashyap999 <anuragkashyap701153@gmail.com>
2026-04-02 22:36:37 +0530

    done

--- src/server.js
+++ src/server.js
@@ -25,7 +25,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 
 app.use('/api/v1/user', usersRoute);
